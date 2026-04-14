8e2794d4be7af705b96d8cac5bae28b3988af180
Kumar Y <kumar@Kumars-MacBook-Pro.local>
2026-04-13 21:33:17 -0700

    updated webpage test

--- tests/LaunchWebPageTest.spec.js
+++ tests/LaunchWebPageTest.spec.js
@@ -4,7 +4,7 @@ test('Launch google page', async ({browser})=>
 {
     const context =await browser.newContext();
     const page =await context.newPage();
-     page.goto("https://www.google.com/");
+     await page.goto("https://www.google.com/");
 });
 // test('Launch page', async ({page})=>
 // {
